9ea4bfcedff57878a6c317ccb90fc70bce82e593
Christopher Weber <chrisrrweber@gmail.com>
2018-08-20 22:36:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared = []
-  array.each do { | numbers | squared << numbers 
+  array.each do { | numbers | squared << numbers ** 2}
     numbers ** numbers
   return array
   end
